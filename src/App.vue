0224802c151a72e61c749896e845dc56246bce3a
maxime <maxime.theveneau@gmail.com>
2024-05-23 17:43:23 +0200

    test auto-deploy

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Github pages auto deploy test" />
+      <HelloWorld msg="Github pages auto-deploy test" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
