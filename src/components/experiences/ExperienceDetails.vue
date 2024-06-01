decaec802f1cb9f840fa9c5c9b0409153fe50f50
maxime <maxime.theveneau@gmail.com>
2024-06-01 17:49:28 +0200

    update AboutView.vue

--- src/components/experiences/ExperienceDetails.vue
+++ src/components/experiences/ExperienceDetails.vue
@@ -1,11 +1,44 @@
 <script>
-
+export default {
+  name: "ExperienceDetails",
+  props: ['experience']
+}
 </script>
 
 <template>
-
+  <div class="card">
+    <routerLink to="/experiences" style="text-decoration: none; color: white">
+      <v-btn prepend-icon="mdi-arrow-left" variant="plain" text="retour"></v-btn>
+    </routerLink>
+    <v-card>
+      <v-card-title>{{experience.title}}</v-card-title>
+      <v-card-subtitle class="mb-5">{{experience.startTime}} - {{experience.endTime}}</v-card-subtitle>
+      <v-card-subtitle>{{experience.place.name}}</v-card-subtitle>
+      <v-btn
+          v-if="experience.place && experience.place.website"
+          prepend-icon="mdi-web"
+          variant="text"
+          color="primary"
+          :href="experience.place.website"
+          text="site web"
+      ></v-btn>
+      <v-card-text>{{experience.description}}</v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <style scoped>
-
+.card *{
+  white-space: pre-line;
+}
+.card{
+  width: 60%;
+  margin-left: 20%;
+}
+@media (max-width: 800px) {
+  .card{
+    width: 90%;
+    margin-left: 5%;
+  }
+}
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,113 @@
 <script>
-
+export default {
+  name: 'AboutView',
+  methods:{
+    goTo(path){
+      this.$router.push(path);
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <h1 class="text-primary">A propos de moi</h1>
+  <div class="container">
+    <div class="avatar">
+      <v-avatar image="images/me.jpeg" size="20vw"></v-avatar>
+      <v-list-item
+          class="text-white"
+          title="Maxime Théveneau"
+          subtitle="Etudiant développeur informatique"
+      ></v-list-item>
+    </div>
+    <div class="content">
+      <h2>Bienvenue sur mon Portfolio !</h2><br>
+      <p>
+        Je m'appelle Maxime et je suis étudiant en développement informatique passionné par les technologies et l'innovation. Actuellement en 3ème année de BUT Informatique à l'IUT de Belfort, je me spécialise dans le développement de logiciels et la création d'applications web et mobiles.
+      </p>
+      <br>
+      <h3>Venez me découvrir :</h3>
+      <v-btn prepend-icon="mdi-arrow-right" variant="plain" color="primary" @click="goTo('/projects')">Mes projets</v-btn>
+      <v-btn prepend-icon="mdi-arrow-right" variant="plain" color="primary" @click="goTo('/experiences')">Mon expérience</v-btn>
+      <v-btn prepend-icon="mdi-arrow-right" variant="plain" color="primary" @click="goTo('/competences')">Mes compétences</v-btn>
+      <br><br>
+      <v-btn
+          prepend-icon="mdi-email"
+          variant="text"
+          density="compact"
+          color="grey"
+          text="maxime.theveneau@gmail.com"
+          href="mailto:maxime.theveneau@gmail.com?subject=Contact from Portfolio&body=Bonjour%20Maxime,%0D%0A%0D%0A%0D%0A"
+      ></v-btn><br>
+      <a href="files/CV_THEVENEAU_Maxime.pdf" download="CV_THEVENEAU_Maxime.pdf">
+        <v-btn
+            prepend-icon="mdi-file-document"
+            variant="text"
+            density="compact"
+            color="grey"
+            text="CV"
+        ></v-btn>
+      </a><br>
+      <v-btn
+          prepend-icon="mdi-linkedin"
+          variant="text"
+          density="compact"
+          color="#126BC4"
+          text="Linkedin"
+          href="https://www.linkedin.com/in/maxime-theveneau-401462219/"
+          target="_blank"
+      ></v-btn><br>
+      <v-btn
+          prepend-icon="mdi-github"
+          variant="text"
+          density="compact"
+          color="#6F6EF1"
+          text="Github"
+          href="https://github.com/Maxime90100"
+          target="_blank"
+      ></v-btn>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  height: 90vh;
+  display: flex;
+}
 
+.avatar {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 40%;
+}
+
+.content {
+  padding-right: 10%;
+  align-content: center;
+  text-align: justify;
+  width: 60%;
+}
+@media (max-width: 1100px) {
+  .container {
+    display: block;
+    padding: 50px;
+  }
+  .avatar{
+    width: 100%;
+  }
+  .content{
+    width: 100%;
+    text-align: center;
+    padding-right: 0;
+    margin-top: 20px;
+  }
+}
+@media (max-width: 500px) {
+  .container {
+    padding: 10px;
+  }
+}
 </style>
